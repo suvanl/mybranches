935157d12b0c62826047f03b623e93dc89bf48de
suvanl <32813609+suvanl@users.noreply.github.com>
2025-01-19 12:56:19 +0000

    Refactor result formatting and logic

--- mybranches.go
+++ mybranches.go
@@ -14,10 +14,11 @@ import (
 type model struct {
 	branches    []string
 	cursorIndex int
-	selected    map[int]struct{}
-	switching   bool
-	switchMsg   string
+	selected    string
 }
+
+const deselectedIndicator = "( )"
+const selectedIndicator = "(*)"
 
 func (m model) Init() tea.Cmd {
 	return nil
@@ -43,19 +44,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 
 		case "enter", " ": // spacebar is represented by space char
-			_, ok := m.selected[m.cursorIndex]
-			if ok {
-				// Remove the entry from the map, it should no longer be selected
-				delete(m.selected, m.cursorIndex)
-			} else {
-				m.selected[m.cursorIndex] = struct{}{}
-
-				// Switch to the selected branch
-				m.switching = true
-				out := switchBranch(m.branches[m.cursorIndex])
-				m.switchMsg = out
-				return m, tea.Quit
-			}
+			m.selected = m.branches[m.cursorIndex]
+			return m, tea.Quit
 		}
 	}
 
@@ -66,46 +56,27 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 //
 // There's no need to implement redrawing logic - bubbletea takes care of redrawing for us.
 func (m model) View() string {
-	s := fmt.Sprintf("Branches containing '%s'\n\n", getUsernamePattern())
+	builder := strings.Builder{}
+	fmt.Fprintf(&builder, "Branches containing '%s'\n\n", getUsernamePattern())
 
-	if m.switching {
-		return m.switchMsg
-	}
-
-	for i, branch := range m.branches {
-		// Render cursor if the current item is selected
-		cursor := " "
+	for i := range m.branches {
 		if m.cursorIndex == i {
-			cursor = ">"
+			builder.WriteString(selectedIndicator + " ")
+		} else {
+			builder.WriteString(deselectedIndicator + " ")
 		}
-
-		// Render checkmark (x) for selected choice
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-
-		// Render row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, branch)
+		builder.WriteString(m.branches[i])
+		builder.WriteString("\n")
 	}
 
-	s += "\nPress q to quit\n"
+	builder.WriteString("\n(press q to quit)\n")
 
-	return s
-}
-
-func main() {
-	program := tea.NewProgram(initialState())
-	if _, err := program.Run(); err != nil {
-		fmt.Printf("Something went wrong: %v", err)
-		os.Exit(1)
-	}
+	return builder.String()
 }
 
 func initialState() model {
 	return model{
 		branches: findBranches(getUsernamePattern()),
-		selected: make(map[int]struct{}),
 	}
 }
 
@@ -155,4 +126,19 @@ func switchBranch(branchName string) string {
 	}
 
 	return string(out[:])
+}
+
+func main() {
+	program := tea.NewProgram(initialState())
+	m, err := program.Run()
+
+	if err != nil {
+		fmt.Printf("Something went wrong: %v", err)
+		os.Exit(1)
+	}
+
+	if m, ok := m.(model); ok && m.selected != "" {
+		switchOut := switchBranch(m.selected)
+		fmt.Printf("\n---\n\n%s\n", switchOut)
+	}
 }
