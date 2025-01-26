8bcf497acbe36bbb87a52f055860ea41ce19e4f5
suvanl <32813609+suvanl@users.noreply.github.com>
2025-01-26 22:40:08 +0000

    clipboard_test: Be more descriptive in test names

--- clipboard_test.go
+++ clipboard_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestPlatformSpecificClipboardImplementation(t *testing.T) {
-	t.Run("when OS is darwin", func(t *testing.T) {
+	t.Run("when OS is darwin, clipboard is instance of DarwinClipboard", func(t *testing.T) {
 		want := DarwinClipboard{}
 		got := getPlatformClipboard("darwin")
 
@@ -12,7 +12,7 @@ func TestPlatformSpecificClipboardImplementation(t *testing.T) {
 		}
 	})
 
-	t.Run("when OS is linux", func(t *testing.T) {
+	t.Run("when OS is linux, clipboard is not supported", func(t *testing.T) {
 		got := getPlatformClipboard("linux")
 
 		if got != nil {
@@ -20,7 +20,7 @@ func TestPlatformSpecificClipboardImplementation(t *testing.T) {
 		}
 	})
 
-	t.Run("when OS is windows", func(t *testing.T) {
+	t.Run("when OS is windows, clipboard is not supported", func(t *testing.T) {
 		got := getPlatformClipboard("windows")
 
 		if got != nil {
