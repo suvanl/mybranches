f30f3ae995ea9477009390b6983eccb6f1d7e1f1
Suvan Leelasena <hello@suvan.dev>
2025-02-06 22:09:27 +0000

    windows: Ignore domain in username (#17)

--- git.go
+++ git.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os/exec"
 	"os/user"
+	"runtime"
 	"strings"
 )
 
@@ -51,8 +52,23 @@ func getUsernamePattern() string {
 		return defaultUsername
 	}
 
+	// Here we go with Windows being funny
+	if runtime.GOOS == "windows" {
+		return withoutDomain(user.Username)
+	}
+
 	// Different branch naming conventions exist, but all usually start with the author's name.
 	// The character after this often differs (":", "/", "-" are commonly used), so we won't include it in default pattern.
 	// If needed, it can be included in the value provided for the `--pattern` flag.
 	return user.Username
+}
+
+// This should only affect Windows, where the username format is DOMAIN\username
+func withoutDomain(withPossibleDomain string) string {
+	split := strings.Split(withPossibleDomain, "\\")
+	if len(split) == 2 {
+		return split[1]
+	}
+
+	return withPossibleDomain
 }
