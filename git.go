a53b387e81be9b83d2a80e50380102f35d610b9d
suvanl <32813609+suvanl@users.noreply.github.com>
2025-02-06 22:00:00 +0000

    Fix domain in Windows username

--- git.go
+++ git.go
@@ -54,5 +54,15 @@ func getUsernamePattern() string {
 	// Different branch naming conventions exist, but all usually start with the author's name.
 	// The character after this often differs (":", "/", "-" are commonly used), so we won't include it in default pattern.
 	// If needed, it can be included in the value provided for the `--pattern` flag.
-	return user.Username
+	return withoutDomain(user.Username)
+}
+
+// This should only affect Windows, where the username format is DOMAIN\username
+func withoutDomain(withPossibleDomain string) string {
+	split := strings.Split(withPossibleDomain, "\\")
+	if len(split) == 2 {
+		return split[1]
+	}
+
+	return withPossibleDomain
 }
