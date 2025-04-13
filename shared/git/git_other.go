8bc48d45aecbd9494e1ec8bc3582910c446d6a49
suvanl <32813609+suvanl@users.noreply.github.com>
2025-04-13 18:28:09 +0100

    comment

--- shared/git/git_other.go
+++ shared/git/git_other.go
@@ -22,6 +22,7 @@ func DeleteBranchesNotOnRemote(dryRun bool) (int, error) {
 func getGoneBranches(dryRun bool) ([]string, error) {
 	// Introduces a dependency on sh, which may symlink to different shells depending on the platform this is running on.
 	// This is simpler than programmatically piping the commands between each other; let's delegate that work for now.
+	// If it starts to lead to inconsistent behaviour across platforms, might be a good idea to revisit this.
 
 	cmd := "git branch -vv | grep 'gone]' | awk '{print $1}'"
 	if !dryRun {
