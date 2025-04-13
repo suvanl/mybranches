e1be0d54adc00e719473a717a5c0885fca897a38
suvanl <32813609+suvanl@users.noreply.github.com>
2025-04-13 15:48:39 +0100

    keep it simple

--- shared/git/git_other.go
+++ shared/git/git_other.go
@@ -3,9 +3,6 @@
 package git
 
 import (
-	"bufio"
-	"bytes"
-	"log"
 	"os/exec"
 	"strings"
 )
@@ -22,82 +19,18 @@ func DeleteBranchesNotOnRemote() (int, error) {
 }
 
 func getGoneBranches() ([]string, error) {
-	branch := exec.Command("git", "branch", "-vv")
-	grep := exec.Command("grep", "'gone]'")
-	awk := exec.Command("awk", "'{print $1}'")
-
-	// Set up pipes between commands
-
-	branchOut, err := branch.StdoutPipe()
+	// Introduces a dependency on sh, which may symlink to different shells depending on the platform this is running on.
+	// This is simpler than programmatically piping the commands between each other; let's delegate that work for now.
+	out, err := exec.Command("sh", "-c", "git branch -vv | grep 'gone]' | awk '{print $1}'").CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
 
-	grepIn, err := grep.StdinPipe()
-	if err != nil {
-		return nil, err
+	outStr := strings.TrimSpace(string(out[:]))
+	if outStr == "" {
+		return []string{}, nil
 	}
 
-	grepOut, err := grep.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-
-	awkIn, err := awk.StdinPipe()
-	if err != nil {
-		return nil, err
-	}
-
-	// Start commands
-
-	if err := branch.Start(); err != nil {
-		return nil, err
-	}
-
-	if err := grep.Start(); err != nil {
-		return nil, err
-	}
-
-	if err := awk.Start(); err != nil {
-		return nil, err
-	}
-
-	// Join pipes
-
-	go func() {
-		defer grepIn.Close()
-		if _, err := bufio.NewReader(branchOut).WriteTo(grepIn); err != nil {
-			log.Fatalln(err)
-		}
-	}()
-
-	go func() {
-		defer awkIn.Close()
-		if _, err := bufio.NewReader(grepOut).WriteTo(awkIn); err != nil {
-			log.Fatalln(err)
-		}
-	}()
-
-	// Finish commands + collect output
-
-	var awkOut bytes.Buffer
-	awk.Stdout = &awkOut
-
-	if err := branch.Wait(); err != nil {
-		return nil, err
-	}
-
-	// ! fixme
-	if err := grep.Wait(); err != nil {
-		return nil, err
-	}
-
-	if err := awk.Wait(); err != nil {
-		return nil, err
-	}
-
-	output := strings.TrimSpace(awkOut.String())
-	branchNames := strings.Split(output, "\n")
-
+	branchNames := strings.Split(outStr, "\n")
 	return branchNames, nil
 }
