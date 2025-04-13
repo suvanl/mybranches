5bf729b8dd7e65d05a4b72ea82a88645b573edc3
Suvan Leelasena <hello@suvan.dev>
2025-04-13 18:53:59 +0100

    Move git functions to shared module (#24)

--- shared/git/git.go
+++ shared/git/git.go
@@ -1,9 +1,59 @@
 package git
 
-import "os/exec"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
 
 // git fetch --prune
 func FetchPrune() error {
 	_, err := exec.Command("git", "fetch", "--prune").CombinedOutput()
 	return err
+}
+
+// Returns branch names found by the `git branch --list <pattern> --format %(refname:short)` command.
+func FindBranches(pattern string) []string {
+	globPattern := fmt.Sprintf("%s*", pattern)
+	out, err := exec.Command("git", "branch", "--list", globPattern, "--format", "%(refname:short)").CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error finding branches: %v\n", err)
+	}
+
+	fromBytes := string(out[:])
+	branches := strings.Split(fromBytes, "\n")
+
+	// Last element will be an empty string, let's just drop it here
+	return branches[:len(branches)-1]
+}
+
+func GetCurrentBranchName() string {
+	out, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error getting current branch: %v\n", err)
+	}
+
+	fromBytes := string(out[:])
+	return strings.Split(fromBytes, "\n")[0]
+}
+
+// Returns the output of the `git switch` command
+func SwitchBranch(branchName string) string {
+	out, err := exec.Command("git", "switch", branchName).CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error switching branch: %v\n", err)
+	}
+
+	return string(out[:])
+}
+
+// Returns the output of the `git branch -D <branch>` command
+func DeleteBranch(branchName string) string {
+	out, err := exec.Command("git", "branch", "-D", branchName).CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error deleting branch: %v\n", err)
+	}
+
+	return string(out[:])
 }
---
+++ user.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os/user"
+	"runtime"
+	"strings"
+)
+
+// Returns the default pattern to use when finding/listing branches
+func getUsernamePattern() string {
+	user, err := user.Current()
+
+	if err != nil {
+		defaultUsername := "user"
+		fmt.Printf("Failed to determine your username. Defaulting to %s.", defaultUsername)
+		return defaultUsername
+	}
+
+	// Here we go with Windows being funny
+	if runtime.GOOS == "windows" {
+		return withoutDomain(user.Username)
+	}
+
+	// Different branch naming conventions exist, but all usually start with the author's name.
+	// The character after this often differs (":", "/", "-" are commonly used), so we won't include it in default pattern.
+	// If needed, it can be included in the value provided for the `--pattern` flag.
+	return user.Username
+}
+
+// This should only affect Windows, where the username format is DOMAIN\username
+func withoutDomain(withPossibleDomain string) string {
+	split := strings.Split(withPossibleDomain, "\\")
+	if len(split) == 2 {
+		return split[1]
+	}
+
+	return withPossibleDomain
+}
