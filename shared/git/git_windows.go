9e203fae7b32a529f03bd3cfc9dd3b105e4e6a5e
suvanl <32813609+suvanl@users.noreply.github.com>
2025-04-13 18:27:04 +0100

    Capitalisation

--- shared/git/git_windows.go
+++ shared/git/git_windows.go
@@ -5,6 +5,6 @@ package git
 import "log"
 
 func DeleteBranchesNotOnRemote(dryRun bool) (int, error) {
-	log.Fatalln("DeleteBranchesNotOnRemote: windows not yet supported")
+	log.Fatalln("DeleteBranchesNotOnRemote: Windows not yet supported")
 	return -1, nil
 }
